b0a38c7c2d8b3c453a1a6c8eeae2957a454fb01e
sibelephant <khalidibrahim@student.oauife.edu.ng>
2025-09-12 19:27:51 +0100

    Refactor AuthLayout to use dynamic link for sign-in and sign-up navigation

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,19 +1,26 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
+  const pathname = usePathname();
+  const isSignIn = pathname === "/sign-in";
   return (
     <main className="bg-neutral-100 min-h-screen ">
       <div className="mx-auto max-w-screen-2xl p-4">
         <nav className="flex justify-between items-center">
           <Image src="/logo.png" alt="logo" width={152} height={56} />
           <div className="flex items-center gap-2">
-            <Button variant={"secondary"} className="">
-              Sign Up
+            <Button asChild variant={"secondary"} className="">
+              <Link href={isSignIn ? "/sign-up" : "/sign-in"}>
+                {pathname === "/sign-in" ? "Sign Up" : "LogIn"}
+              </Link>
             </Button>
           </div>
         </nav>
